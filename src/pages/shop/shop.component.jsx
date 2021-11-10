fd73c67a07c129c2c88bdd1670783f6ad2c88910
Harshali Matte <harshalimatte10@gmail.com>
2021-11-10 16:59:21 +0000

    updating the collection preview and collection item components with styling

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,27 @@
 import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data';
+import SHOP_DATA from './shop.data.js';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collection: SHOP_DATA 
-          }
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
 
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map( ({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-                                {/* {
-                    collections.map(({id, ...otherCollectionProps})=>(
-                        <CollectionPreview key='id' {...otherCollectionProps}/>  
-                    ))
-                } */}
-            
-            </div>
-        )
-    }
-   
+  render() {
+    const { collections } = this.state;
     
+    return (
+        <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+         <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
