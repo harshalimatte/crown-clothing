fa45ed080871647cdc8ca3d3b10014a75c71799b
unknown <harshalimatte10@gmail.com>
2021-11-12 11:42:51 +0000

    getting the user logged in information from firebase doc

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,10 +7,37 @@ const config = {
     authDomain: "crwn-db-8e653.firebaseapp.com",
     projectId: "crwn-db-8e653",
     storageBucket: "crwn-db-8e653.appspot.com",
-    messagingSenderId: "401821733127",
+    messagingSenderId: "401821733127", 
     appId: "1:401821733127:web:a5baca4a5ab509502fb5a2",
     measurementId: "G-5LWRTTP3XH"
 };
+
+export const createUserProfileDocument = async(userAuth, additionalData) =>{
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get( );
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+
+        } catch(error){
+            console.log('error creating user', error.message)
+        }
+    }
+    
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
